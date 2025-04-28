5fa6a87629c2769a0e84e16aeb90f6f66e83e366
dulalaavas <144573527+dulalaavas@users.noreply.github.com>
2025-04-28 11:59:07 +0545

    Some changes.

--- apr28.java
+++ apr28.java
@@ -5,11 +5,18 @@ public class apr28{
     static void mul(int a, int b){
         System.out.println(a*b);
     }
-
+    static void greet(){
+        System.out.println("Hello!");
+    }
+    static int sub(){
+        int a = 5, b = 6;
+        return a-b;
+    }
     public static void main(String[] args){
         int sum = add(5,6);
         System.out.println(sum);
         mul(5,6);
-
+        greet();
+        System.out.println(sub());
     }
-}
+} 
