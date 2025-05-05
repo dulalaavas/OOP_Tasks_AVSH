68d6a19d2e2b5c50cbc9b6a3fc083a864f128a60
dulalaavas <144573527+dulalaavas@users.noreply.github.com>
2025-05-05 12:03:20 +0545

    Done another question

--- May5.java
+++ May5.java
@@ -10,13 +10,31 @@ public class May5{
         return age;    
     }
 
+    static double SimpleInterest(double pi, double ti, double ra){
+        double si = (pi*ti*ra)/100;
+
+        System.out.print(si);
+
+        return si;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter your age: ");
         int age = sc.nextInt();
 
+        System.out.print("Enter Principle: ");
+        double pi = sc.nextDouble();
+        System.out.print("Enter Time: ");
+        double ti = sc.nextDouble();
+        System.out.print("Enter Rate: ");
+        double ra = sc.nextDouble();
+        
+
         Vote(age);
+
+        SimpleInterest(pi, ti, ra);
 
         sc.close();
     }
