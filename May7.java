19715327d03c64e85936619e0afe0bd196e5d851
dulalaavas <144573527+dulalaavas@users.noreply.github.com>
2025-05-07 12:36:00 +0545

    Implement main method and array initialization in May7.java

--- May7.java
+++ May7.java
@@ -1,3 +1,12 @@
 public class May7 {
-    
+    public static void main(String[] args) {
+        int[] array1 = new int[5];
+        int[] array2 = {1, 2, 3, 4, 5};
+        for (int num : array2) {
+            System.out.println("array2 element: " + num);
+        }
+        int len = array1.length;
+        array1[4] = 2;
+        System.out.println("array1[4]: " + array1[4]);
+    }
 }
