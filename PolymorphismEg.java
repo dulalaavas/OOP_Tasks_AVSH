25479389325e5d354c7bd0da810e5ea5c1f21188
dulalaavas <144573527+dulalaavas@users.noreply.github.com>
2025-06-15 12:23:58 +0545

    added 3rd method

--- PolymorphismEg.java
+++ PolymorphismEg.java
@@ -2,13 +2,17 @@
 // objects fo different classes to be treated as objects of same superclass
 // 1. Method Overloading
 // 2. Method Overriding
-public class PolimorphismEg {
+public class PolymorphismEg {
     public static void add(int a, int b) {
         System.out.println("two params");
         System.out.println(a+b);
     }
     public void add (int a, int b, int c) {
         System.out.println("Three params");
-        System.out.println("a+b+c");
+        System.out.println(a+b+c);
+    }
+    public void add (int a, int b, String name) {
+        System.out.println("Three params");
+        System.out.println(a+b);
     }
 }
